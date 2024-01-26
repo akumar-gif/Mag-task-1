593319370f440d5b0239d5561e0d1d3fc8a47fda
akumar-gif <akumark503@gmail.com>
2024-01-26 10:08:24 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1" 
+}
+
+resource "aws_instance" "my_instance" {
+  ami             = "ami-0005e0cfe09cc9050" 
+  instance_type   = "t2.micro"
+  key_name        = "Mag" 
+  #iam_instance_profile        = aws_iam_instance_profile.session-manager-instance.id
+  security_groups = ["sg-03e55f0b8518b357b"]
+  #subnet_id = aws_subnet.main-sub.id
+
+  tags = {
+    Name = "Created with Jenkins"
+  }
+}
